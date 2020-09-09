94e4c1155e6c742fcf7335d7b73f241294c425c5
Ankit Raj <ankitatiiitr@gmail.com>
2020-09-09 15:09:04 +0530

    Added notice api (#13)
    * Created Notice api
    
    * package-lock.json not required here
    
    * notice get route modification

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var noticeRouter = require('./routes/notice');
+
+mongoose.connect(process.env.MONGO_DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
 
 var app = express();
 
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/notice', noticeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/models/Notice.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const noticeSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    createdAt: {
+        required: true,
+        type: Date,
+        default: Date.now()
+    },
+    description: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    pdfPath: {
+        type: String,
+        trim: true,
+    },
+    eventLink: {
+        type: String,
+    }
+});
+
+module.exports = new mongoose.model('Notice', noticeSchema);
