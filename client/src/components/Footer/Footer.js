f7297722e1263c1d1d473942a9555c51d4ba4e54
tusharjain0022 <tusharjain0022@gmail.com>
2020-09-10 10:48:25 +0530

    footer component added to frontend

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.css';
+import Hog from './img/hog.png';
 
 function Footer() {
   return (
     <div className="footer-component">
-      <h1>This is footer.</h1>
-    </div>
+      {/* Contact Us Section Starts Here */}
+      <div className="container">
+         <div className="row">
+           <div className="col-12 text-center text-white my-3">
+             <hr></hr>
+             <h3> Contact Us</h3>
+             <div className="mt-5 mb-3">
+                  <a className=" btn text-white" rel="noopener noreferrer" href="https://"><i  className="iconer fab fa-linkedin fa-3x"></i></a>
+                  <a className=" btn text-white" rel="noopener noreferrer" href="https://"><i  className=" iconer fab fa-github fa-3x"></i></a>
+                  <a className=" btn text-white" rel="noopener noreferrer" href="mailto:iiitranchi@contact.info"><i  className="iconer fas fa-envelope fa-3x"></i></a>
+                  <a className=" btn text-white" rel="noopener noreferrer" href="https://"><i  className="iconer fab fa-facebook fa-3x"></i></a>
+                  <a className=" btn text-white" rel="noopener noreferrer" href="https://"><i  className="iconer fab fa-instagram fa-3x"></i></a>
+                  <a className=" btn text-white" rel="noopener noreferrer" href="https://"><i  className="iconer fab fa-youtube fa-3x"></i></a>
+             </div>
+           </div>
+         </div>
+      </div>
+      {/* Contact Us Section Ends here */}
+    
+    {/* Footer starts here */}
+    <footer className="footer pb-0">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-3 offset-md-1 links">
+                        <h5 className="font-weight-bold">Links</h5>
+                        <a className="  text-white"  href="https://">Home</a><br></br>
+                        <a className="  text-white"  href="https://">About</a><br></br>
+                        <a className="  text-white"  href="https://">Socities</a><br></br>
+                        <a className="  text-white"  href="https://">Projects</a><br></br>
+                        <a className="  text-white"  href="mailto:iiitranchi@contact.info">IIITRanchi</a>
+                </div>
+                <div className="col-12 col-md-4 links">
+                    <hr className="d-md-none"></hr>
+                    <h5 className="font-weight-bold">Our Address</h5>
+                    <address>
+                        <ul className="list-unstyled">
+                            <li className="institute-address">Indian Institute of Information Technology,&nbsp;Ranchi</li>
+                            <li className="institute-address">Sirkha Toli, Kali Nagar, Namkum, </li>
+                            <li className="institute-address">Ranchi, Jharkhand 834010</li>
+                            <li className="institute-address">Tel : 123-24578</li>
+                            <li className="institute-address">Mail : <a className="footer-a" rel="noopener noreferrer" href="mailto:iiitranchi@contact.info">iiitranchi@contact.info</a></li>
+                        </ul><br></br>
+		                </address>
+                </div>
+                {/* HG logo will be rendered only on screen-width greater than 768px */}
+                <div className="col-12 col-md-4 d-none d-md-block"><img src={Hog} className="logo"></img></div>
+            </div>
+        </div>
+
+        <div className="container-fluid">
+            <div className="row justify-content-center mt-2 mb-0 pt-3">
+                <div  className="col-2 col-sm-1 "><button id="dev" className=" btn text-white " data-toggle="modal" data-target="#exampleModal"><i className="iconer fas fa-laptop-code fa-2x"></i><small>Developers</small></button></div>
+                <div className="col-6 col-sm-10 text-center">
+                   <hr id="footer-hr"></hr>
+                   <p className="text-white ">House Of Geeks © Copyright 2020 </p>
+                </div>
+                <div className="col-2 col-sm-1"><a id="up" className=" btn text-white " rel="noopener noreferrer" ><i className="iconer fas fa-chevron-circle-up fa-2x" id="to-top" onClick={window.scrollTo({top:0,behavior:'smooth'})}></i><br></br><small>Back to Top</small></a></div>
+            </div>
+        </div>
+        
+   </footer>
+   {/* Footer Ends here */}
+  </div>
   );
 }
 
