5dbc5f1465ee456a7ad94990319ccd34854e063e
EmperorYP7 <pandeyyash04@gmail.com>
2020-09-11 20:18:51 +0530

    expanding home page

---
+++ client/src/components/Home/Notice/Notice.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Notice.css';
+
+function Notice() {
+  return (
+    <div className="notice-section">
+      <h1>This is the Notice section.</h1>
+    </div>
+  );
+}
+
+export default Notice;
