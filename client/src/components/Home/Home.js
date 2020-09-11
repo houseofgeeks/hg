b43a47d9abc4c81b404a279c5dd67382e57b10ed
EmperorYP7 <pandeyyash04@gmail.com>
2020-09-11 23:07:53 +0530

    minor fixes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
-<<<<<<< HEAD
 import LandingPage from './LandingPage/LandingPage';
 import Notice from './Notice/Notice';
 import CommunitySection from './CommunitySection/CommunitySection';
 import AboutUs from './AboutUs/AboutUs';
 import PartnersSection from './PartnersSection/PartnersSection';
+import shadow from './blue_blur.png';
 
 function Home() {
   return (
     <div className="home-component">
-      <h1>This is the Home page.</h1>
+      <img className="lander-img img-fluid" src={shadow} alt="lander"/>
       <LandingPage />
       <AboutUs />
       <Notice />
       <CommunitySection />
       <PartnersSection />
-=======
-import shadow from './blue_blur.png';
-function Home() {
-  return (
-    <div className="home-component">
-      <img className="lander-img img-fluid" src={shadow} alt="lander"/>
->>>>>>> 1e9a8568958e0b1dd9e03ab4e46ef594ffaa3ef4
     </div>
-  );
+    );
 }
 
 export default Home;
