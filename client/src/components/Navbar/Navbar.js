fe2ebc8a7f25f7997f0df33df4eb8ac4a476bbe4
Rishav Mazumdar <58086511+RishavMz@users.noreply.github.com>
2020-09-11 17:13:26 +0530

    Styled navbar (#20)
    * Added bootstrap and jQuery
    
    * Navbar styling and fixing active element (#16)
    
    * Minor changes
    
    * Update index.html
    
    * Update index.html
    
    * Navbar test
    
    * Navbar opacity update
    
    Testing after removing the opacity styling.
    
    * Search bar removed in mobile view

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,15 +2,27 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import logo from './hog.png';
 
 function Navbar() {
-    const [active, setActive] = useState('Home');
+    const currentloc = String(window.location);                             //Gets current URL
+    let currenttab = currentloc.substring(currentloc.lastIndexOf("/")+1);   //Gets current element from navbar
+    
+    if(currenttab[currenttab.length-1] === '?')                        //When search bar is used, preventing bug of nothing being active in navbar
+    {
+        currenttab = currenttab.substring(0,currenttab.length-1);           //Removing ? sign when search bar used
+    }    
+    
+    if(currenttab.length === 0 ){                                           //When visiting home section
+        currenttab = "home";
+    }
+    const [active, setActive] = useState(currenttab);
     return (
         <div className="navbar-component">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
                 <Link to ="/" id="navLink">
-                    <div className="navbar-brand">House of Geeks</div>
+                    <div className="navbar-brand"><img src={logo} className="logo" alt = "HouseOfGeeks logo"/></div>
                 </Link>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" 
@@ -24,49 +36,55 @@ function Navbar() {
 
                         <Link to ="/" id="navLink">
                             <hr className="d-md-none" />
-                            <li className={(active==='Home')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
-                            onClick={() => setActive('Home')}>
+                            <li className={(active==='home')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
+                            onClick={() => setActive('home')}>
                                 Home
                             </li>
                         </Link>
 
                         <Link to="/event" id="navLink">
                             <hr className="d-md-none" /> 
-                            <li className={(active==='Event')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"}
-                             onClick={() => setActive('Event')}>
-                                Events
+                            <li className={(active==='event')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"}
+                             onClick={() => setActive('event')}>
+                                Event
                             </li>
                         </Link>
 
                         <Link to ="/community" id="navLink">
                             <hr className="d-md-none" /> 
-                            <li className={(active==='Community')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
-                            onClick={() => setActive('Community')}>
+                            <li className={(active==='community')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
+                            onClick={() => setActive('community')}>
                                 Community
                             </li>
                         </Link>
 
                         <Link to ="/members" id="navLink">
                             <hr className="d-md-none" /> 
-                            <li className={(active==='Members')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
-                            onClick={() => setActive('Members')}>
+                            <li className={(active==='members')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
+                            onClick={() => setActive('members')}>
                                 Members
                             </li>
                         </Link>
 
                         <Link to ="/sponsors" id="navLink">
                             <hr className="d-md-none" /> 
-                            <li className={(active==='Sponsors')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
-                            onClick={() => setActive('Sponsors')}>
+                            <li className={(active==='sponsors')?"nav-item mx-3 px-3 active":"nav-item mx-3 px-3"} 
+                            onClick={() => setActive('sponsors')}>
                                 Sponsors
                             </li>
                         </Link>
 
                     </ul>
+                    
+                    <form className="form-inline mx-3 px-3 " id = "searchbar">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    </form>
                 </div>
             </nav>
         </div>
     )
 }
+   
 
 export default Navbar;
+
