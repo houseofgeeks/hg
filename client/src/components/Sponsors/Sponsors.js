8542cece651cd0e127bbb4cc64e5edde5d538759
Abhay Raj <abhayrajsingh1707@gmail.com>
2020-09-13 00:39:09 +0530

    making inditation clear

--- client/src/components/Sponsors/Sponsors.js
+++ client/src/components/Sponsors/Sponsors.js
@@ -5,30 +5,28 @@ import SponserCard from './SponsorsCard';
 
 function Sponsors() {
   return (
-    <>
-     <div className="container-fluid col-10 mx-auto">
-    <div className="my-5">
-    <br/><br/> <br/> <br/> <br/>
-     <h1 className="text-center">Our Partners</h1>
+    <div>
+      <div className="container-fluid col-10 mx-auto">
+        <div className="my-5">
+          <br /><br /> <br /> <br /> <br />
+          <h1 className="text-center">Our Partners</h1>
+        </div>
+        <div className="row">
+          <SponserCard
+            title="Partner"
+          />
+          <SponserCard
+            title="Partner"
+          />
+          <SponserCard
+            title="Partner"
+          />
+          <SponserCard
+            title="Partner"
+          />
+        </div>
+      </div>
     </div>
-    <div className="row">
-    <SponserCard
-      title = "Partner"
-    />
-   <SponserCard
-       title = "Partner"
-   />
-   <SponserCard
-       title = "Partner"
-   />
-   <SponserCard
-       title = "Partner"
-   />
-
-    </div>
-    </div>
-
-    </>
   );
 }
 
--- client/src/components/Sponsors/SponsorsCard.jsx
+++ client/src/components/Sponsors/SponsorsCard.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const SponserCard = (props)=>{
-    return(
-        <>  
-
-  <div className="col-sm-3 mx-auto">
-    <div className="card my-2">
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
+const SponserCard = (props) => {
+  return (
+    <div>
+      <div className="col-sm-3 mx-auto">
+        <div className="card my-2">
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
- 
- 
-        </>
-    )
+  )
 }
 export default SponserCard;
