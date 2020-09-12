42a6edc52f69ccc76849893bf86cfe31f4d90bb9
Yash Pandey (YP) <pandeyyash04@gmail.com>
2020-09-12 16:50:38 +0530

    Refactoring home page (#28)
    * expanding home page
    
    * adding minor tweaks
    
    * minor fixes
    Reviewer @ankiiitraj and @hariom1625

---
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './PartnersSection.css';
+
+function PartnersSection() {
+  return (
+    <div className="partners-section">
+      <h1>This is the Partners section.</h1>
+    </div>
+  );
+}
+
+export default PartnersSection;
