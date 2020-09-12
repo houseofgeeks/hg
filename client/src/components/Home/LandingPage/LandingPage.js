fda590ce45bf4c27caee1855f139f6b7b3dfb77e
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-09-13 01:25:56 +0530

    modifying homepage

--- client/src/components/Home/LandingPage/LandingPage.js
+++ client/src/components/Home/LandingPage/LandingPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LandingPage.css';
+import asteroids from './asteroids.svg';
+import writeup from './writeup.svg';
 
 function LandingPage() {
   return (
-    <div className="landing-page-section">
-      <h1>This is the Landing Page section.</h1>
+    <div className="landing-page-section container-fluid">
+      <div className="writeup">
+        <img className="img-fluid asteroids" src={asteroids} alt="asteroids" />
+        <img className="img-fluid writeup-img" src={writeup} alt="House of Geeks" />
+      </div>
     </div>
   );
 }
