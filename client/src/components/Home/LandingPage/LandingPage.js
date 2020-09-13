0d53fb0c2c3282bf14103d867ad2c3904b542881
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-09-13 15:28:28 +0530

    remaking the landing page

--- client/src/components/Home/LandingPage/LandingPage.js
+++ client/src/components/Home/LandingPage/LandingPage.js
@@ -3,13 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './LandingPage.css';
 import asteroids from './asteroids.svg';
 import writeup from './writeup.svg';
+import surface from './surface.svg';
+import astronaut from './astronaut.svg';
 
 function LandingPage() {
   return (
-    <div className="landing-page-section container-fluid">
+    <div className="landing-page-section">
+      <div className="asteroids">
+        <img className="img-fluid asteroids-img" src={asteroids} alt="asteroids" />
+      </div>
       <div className="writeup">
-        <img className="img-fluid asteroids" src={asteroids} alt="asteroids" />
-        <img className="img-fluid writeup-img" src={writeup} alt="House of Geeks" />
+        <img className="img-fluid writeup-img" src={writeup} alt="House of Geeks"/>
+      </div>
+      <div className="surface">
+        <img className="surface-img" src={surface} alt="surface"/>
+      </div>
+      <div className="astronaut">
+        <img className="astronaut-img" src={astronaut} alt="astronaut"/>
       </div>
     </div>
   );
