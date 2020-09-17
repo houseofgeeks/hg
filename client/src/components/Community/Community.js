ddfd5903fd1de99b018450c5c3b764ab4de4ff8b
unbeat <vandanagupta1499@gmail.com>
2020-09-17 16:47:09 +0530

    Updated  the community section

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Community.css';
+import { Carousel, Card } from "react-bootstrap"
 
 function Community() {
   return (
     <div className="community-component">
-      <h1>This is the community page.</h1>
+      <div className="Society-cards">
+        <h1 className="heading">Societies under Geeks</h1>
+        <Carousel>
+          <Carousel.Item>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+    </Card.Text>
+
+              </Card.Body>
+            </Card>
+
+          </Carousel.Item>
+          <Carousel.Item>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+    </Card.Text>
+
+              </Card.Body>
+            </Card>
+
+          </Carousel.Item>
+          <Carousel.Item>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+    </Card.Text>
+
+              </Card.Body>
+            </Card>
+
+
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
   );
 }
