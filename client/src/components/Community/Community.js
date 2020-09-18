16937b7bca8e165e9e89988c057c45263e0ca705
unbeat <vandanagupta1499@gmail.com>
2020-09-18 20:13:26 +0530

    Update Community.js

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -17,8 +17,19 @@ function Community() {
                 <Card.Text>
                   Some quick example text to build on the card title and make up the bulk of
                   the card's content.
-    </Card.Text>
-
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                 </Card.Text>
               </Card.Body>
             </Card>
 
@@ -31,26 +42,9 @@ function Community() {
                 <Card.Text>
                   Some quick example text to build on the card title and make up the bulk of
                   the card's content.
-    </Card.Text>
-
+                 </Card.Text>
               </Card.Body>
             </Card>
-
-          </Carousel.Item>
-          <Carousel.Item>
-            <Card style={{ width: '18rem' }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-    </Card.Text>
-
-              </Card.Body>
-            </Card>
-
-
           </Carousel.Item>
         </Carousel>
       </div>
