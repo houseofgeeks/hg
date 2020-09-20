3f2e0c9ba786c5d6f40c00c47eba862ffd121920
ankiiitraj <ankitatiiitr@gmail.com>
2020-09-20 16:06:21 +0530

    cleaning up

--- backend/routes/notice.js
+++ backend/routes/notice.js
@@ -4,10 +4,7 @@ var router = express.Router();
 const auth = require("../util/auth");
 const Notice = require("../models/notice.model");
 
-<<<<<<< HEAD
 // Creates a notice
-=======
->>>>>>> 39d976be601be25b3630d1bc8d3a943571da73c3
 router.post("/", auth, function (req, res, next) {
   const notice = new Notice({
     title: req.body.title,
@@ -27,11 +24,7 @@ router.post("/", auth, function (req, res, next) {
     })
     .catch((err) => {
       res.status(500).json({
-<<<<<<< HEAD
         message: "Something went wrong",
-=======
-				message: 'Something went wrong'
->>>>>>> 39d976be601be25b3630d1bc8d3a943571da73c3
       });
       console.log(err);
     });
@@ -41,7 +34,6 @@ router.post("/", auth, function (req, res, next) {
 router.get("/", (req, res, next) => {
   Notice.find({})
     .sort({ createdAt: -1 })
-<<<<<<< HEAD
     .limit(10)
     .then((result) => {
       res.status(200).json({
@@ -61,8 +53,6 @@ router.get("/", (req, res, next) => {
 router.get("/all", (req, res, next) => {
   Notice.find({})
     .sort({ createdAt: -1 })
-=======
->>>>>>> 39d976be601be25b3630d1bc8d3a943571da73c3
     .then((result) => {
       res.status(200).json({
         result: result,
