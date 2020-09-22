06aedf045a3cabb5d68bf80663cb94fcc47560f1
ankiiitraj <ankitatiiitr@gmail.com>
2020-09-22 18:54:31 +0530

    testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    message: 'index'
+  })
 });
 
 module.exports = router;
