10fd2273247f3c3a050a78c353f54f29261eef60
EmperorYP7 <pandeyyash04@gmail.com>
2020-09-22 13:48:40 +0530

    shortening description

--- client/src/components/Home/AboutUs/AboutUs.js
+++ client/src/components/Home/AboutUs/AboutUs.js
@@ -16,9 +16,8 @@ function AboutUs() {
         <div className="content">
           House of Geeks is the Technical Society of Indian Institute of Information Technology,
           Ranchi. Gravity is not just a force, it is the tendency of matter to move towards 
-          where the time flows slower. Following the same principle, we believe that collaborating,
-          mending together minds to make something phenomenal is time travel in itself! 
-          So, buckle up as Captain Geek takes you through our communities and societies!
+          where the time flows slower. We believe that collaborating, mending together 
+          minds to make something phenomenal is time travel in itself!
         </div>
         <Notice />
       </div>
