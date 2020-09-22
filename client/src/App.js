19dd94870b3d35b7579b748ac8ed0fb908b47e23
Hari Om Yadav <66957239+hariom1625@users.noreply.github.com>
2020-09-23 01:17:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Members from "./components/Members/Members";
+import Sponsors from "./components/Sponsors/Sponsors";
+import Event from "./components/Event/Event";
+import Community from "./components/Community/Community";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Error from "./components/Error/Error";
+import Navbar from "./components/Navbar/Navbar";
+import LoginForm from './components/Admin/Login/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <center>Hi!</center>
+    <div className="app-component">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/admin" exact component={LoginForm}/>
+          <Route path="/members" exact component={Members} />
+          <Route path="/sponsors" exact component={Sponsors} />
+          <Route path="/event" exact component={Event} />
+          <Route path="/community" exact component={Community} />
+          <Route path="/" exact component={Home} />
+          <Route path="/" component={Error} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
