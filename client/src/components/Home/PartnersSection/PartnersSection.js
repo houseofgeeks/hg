bdef3ab3f5040952f251985a170c0b403e791245
Ankit Raj <ankitatiiitr@gmail.com>
2020-09-24 01:08:52 +0530

    Another typo fix

--- client/src/components/Home/PartnersSection/PartnersSection.js
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnersSection.css';
 import {PartnersList as partners} from './PartnersList.js';
