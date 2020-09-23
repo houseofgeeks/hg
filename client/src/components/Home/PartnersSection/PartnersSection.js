398e134218c570096111bb7a46a30f9360945f40
Ankit Raj <ankitatiiitr@gmail.com>
2020-09-24 00:56:54 +0530

    coverted class component to dunctional component

--- client/src/components/Home/PartnersSection/PartnersSection.js
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -1,42 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnersSection.css';
-import {PartnersList} from './PartnersList.js';
+import {PartnersList as partners} from './PartnersList.js';
 
-class PartnersSection extends React.Component {
-      constructor(props) {
-            super(props);
-            this.state = {
-                  partners: PartnersList
-
-            }
-      }
-
-      render() {
-            const display = this.state.partners.map((partner) => {
-                  return (<div className="partners-col-1 col-lg-6 col-md-12">
-
-                        <img className="partners-logo" src={`${partner.img}`} alt="partners"/>
-                        <h2 className="logo-name">
-                              {partner.name}
-                        </h2>
-                  </div>);
-            });
-            return (<div className="partners-section container">
-
+const PartnersSection = () => {
+      return (
+            <div className="partners-section container">
                   <div className="partners-box">
                         <div className="partners-title">
                               Partners and Sponsors
                         </div>
                         <div className="partners-img row">
-                              {display}
-
+                              {
+                                    partners.map((partner, idx) => {
+                                          return(
+                                                <div className="partners-col-1 col-lg-6 col-md-12">
+                                                      <img className="partners-logo" src={`${partner.img}`} alt="partners"/>
+                                                      <h2 className="logo-name">
+                                                            {partner.name}
+                                                      </h2>
+                                                </div>
+                                          )     
+                                    });
+                              }
                         </div>
-
                   </div>
-
-            </div>);
-      }
+            </div>
+      );
 }
-
 export default PartnersSection;
