4eded69f866219ea8e3b6885ee49272bbeb3964f
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-09-23 16:56:55 +0530

    update partners section

--- client/src/components/Home/PartnersSection/PartnersSection.js
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnersSection.css';
-import coding from './coding.png';
+import {PartnersList} from './PartnersList.js';
 
-function PartnersSection() {
-      return (<div className="partners-section container">
+class PartnersSection extends React.Component {
+      constructor(props) {
+            super(props);
+            this.state = {
+                  partners: PartnersList
 
-            <div className="partners-box">
-                  <div className="partners-title">
-                        Partners and Sponsors
+            }
+      }
+
+      render() {
+            const display = this.state.partners.map((partner) => {
+                  return (<div className="partners-col-1 col-lg-6 col-md-12">
+
+                        <img className="partners-logo" src={`${partner.img}`} alt="partners"/>
+                        <h2 className="logo-name">
+                              {partner.name}
+                        </h2>
+                  </div>);
+            });
+            return (<div className="partners-section container">
+
+                  <div className="partners-box">
+                        <div className="partners-title">
+                              Partners and Sponsors
+                        </div>
+                        <div className="partners-img row">
+                              {display}
+
+                        </div>
+
                   </div>
-                  <div className="partners-img row">
-                        <div className="partners-col-1 col-lg-6 col-md-12">
-                              <img className="partners-logo" src={coding} alt="coding-blocks"/>
-                        </div>
-                        <div className="partners-col-2 col-lg-6 col-md-12">
 
-                              <img className="partners-logo" src={coding} alt="coding-blocks"/>
-                        </div>
-
-                  </div>
-                  <div className="partners-img row">
-                        <div className="partners-col-1 col-lg-6 col-md-12">
-                              <img className="partners-logo" src={coding} alt="coding-blocks"/>
-                        </div>
-                        <div className="partners-col-2 col-lg-6 col-md-12">
-
-                              <img className="partners-logo" src={coding} alt="coding-blocks"/>
-                        </div>
-
-                  </div>
-            </div>
-
-      </div>);
+            </div>);
+      }
 }
 
 export default PartnersSection;
