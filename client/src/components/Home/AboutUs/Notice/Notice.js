d5e44ec071d28b854e5bc3341c41bdfa8c032237
Ankit Raj <ankitatiiitr@gmail.com>
2020-09-24 20:03:12 +0530

    added api support to notices (#82)
    added api support to notices

--- client/src/components/Home/AboutUs/Notice/Notice.js
+++ client/src/components/Home/AboutUs/Notice/Notice.js
@@ -1,42 +1,77 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import planet from './planet.svg';
-import union from './union.svg';
-import mobilePlanet from './mobile-planet.svg';
-import './Notice.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import planet from "./planet.svg";
+import union from "./union.svg";
+import mobilePlanet from "./mobile-planet.svg";
+import "./Notice.css";
 
 function Notice() {
+  const [notices, setNotice] = useState([
+    { title: "fetching notices", description: " . . ." },
+  ]);
+  useEffect(() => {
+    axios
+      .get("/api/notice")
+      .then((res) => {
+        const { result } = res.data;
+        setNotice([...result]);
+      })
+      .catch((err) => {
+        const result = [];
+        result.push({
+          title: "Something went wrong!",
+          description: "Try reloading. . .",
+        });
+        setNotice(result);
+      });
+  }, []);
+
   return (
     <div className="notice-section">
-      <div className="notice-title">
-          From the Geeks' Satellite..
+      <div className="notice-title">From the Geeks' Satellite..</div>
+      <div className="notice-content">
+        <div className="title">
+          Recieving signals...{" "}
+          <span className="decrypting"> Decrypting...</span>
+          <span className="latest-news"> Latest News...</span>
         </div>
-        <div className="notice-content">
-          <div className="title">
-            Recieving signals... <span className="decrypting"> Decrypting...</span> 
-            <span className="latest-news"> Latest News...</span>
-          </div>
-          <div className="notice-list">
-            <ul>
-              <li>Notice title1</li>
-              <li>Notice title2</li>
-              <li>Notice title3</li>
-              <li>Notice title4</li>
-              <li>Notice title5</li>
-              <li>Notice title6</li>
-            </ul>
-          </div>
-        </div>
-        <div className="planet">
-          <img src={planet} className="planet-img" alt="planet" />
-        </div>
-        <div className="mobile-planet">
-          <img src={mobilePlanet} className="mobile-planet-img" alt="mobile planet" />
-        </div>
-        <div className="union">
-          <img src={union} className="union-img" alt="union"/>
+        <div className="notice-list">
+          <ul>
+            {notices.map((notice, idx) => {
+              return (
+                <li key={idx}>
+                  <h3>{notice.title}</h3>
+                  {notice.description && <p>{notice.description}</p>}
+                  {notice.eventLink && (
+                    <a
+                      rel="noopener noreferrer"
+                      href={`${notice.eventLink}`}
+                      target="_blank"
+                    >
+                      Link
+                    </a>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
+      <div className="planet">
+        <img src={planet} className="planet-img" alt="planet" />
+      </div>
+      <div className="mobile-planet">
+        <img
+          src={mobilePlanet}
+          className="mobile-planet-img"
+          alt="mobile planet"
+        />
+      </div>
+      <div className="union">
+        <img src={union} className="union-img" alt="union" />
+      </div>
+    </div>
   );
 }
 
