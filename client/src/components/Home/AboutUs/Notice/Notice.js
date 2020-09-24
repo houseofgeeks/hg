5954d41f8b8dab3c336f04cf0a564d68f18970d0
tusharjain0022 <tusharjain0022@gmail.com>
2020-09-24 11:18:46 +0530

    Revert "Merge branch 'front-end' of https://github.com/houseofgeeks/hg into front-end"
    This reverts commit 71575fc812e9483b2c2619785cb5288b2bfe389a, reversing
    changes made to c7b43b5fff7011eb879b79e407f6eb723f89f798.

--- client/src/components/Home/AboutUs/Notice/Notice.js
+++ client/src/components/Home/AboutUs/Notice/Notice.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import planet from './planet.svg';
 import union from './union.svg';
@@ -7,21 +6,6 @@ import mobilePlanet from './mobile-planet.svg';
 import './Notice.css';
 
 function Notice() {
-
-  const [notices, setNotice] = useState([{title: 'fetching notices', description:' . . .'}]);
-  useEffect(() => {
-    axios.get('/api/notice')
-    .then(res => {
-      const {result} = res.data;
-      setNotice([...result]);
-    })
-    .catch((err) => {
-      const result = [];
-      result.push({title: 'Something went wrong!', description: 'Try reloading. . .'});
-      setNotice(result);
-    })
-  }, []);
-
   return (
     <div className="notice-section">
       <div className="notice-title">
@@ -34,13 +18,12 @@ function Notice() {
           </div>
           <div className="notice-list">
             <ul>
-              {notices.map((notice, idx) => {
-                return <li key={idx}>
-                  <h3>{notice.title}</h3>
-                  {notice.description && <p>{notice.description}</p>}
-                  {notice.eventLink && <a rel="noopener noreferrer" href={`${notice.eventLink}`} target='_blank'>Link</a>}
-                </li>
-              })}
+              <li>Notice title1</li>
+              <li>Notice title2</li>
+              <li>Notice title3</li>
+              <li>Notice title4</li>
+              <li>Notice title5</li>
+              <li>Notice title6</li>
             </ul>
           </div>
         </div>
