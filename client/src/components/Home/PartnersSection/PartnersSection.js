7ff17fb1d78a1a1237a15fe6341682e93d3e5e47
Ankit Raj <ankitatiiitr@gmail.com>
2020-09-24 15:46:13 +0530

    Revert "Revert "Adding Partners Section (#75)" (#80)"
    This reverts commit 95a40ebac8c2a8640b49037395d4904f77a42e03.

--- client/src/components/Home/PartnersSection/PartnersSection.js
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnersSection.css';
+import {PartnersList as partners} from './PartnersList.js';
 
-function PartnersSection() {
-  return (
-    <div className="partners-section">
-      <h1>This is the Partners section.</h1>
-    </div>
-  );
+const PartnersSection = () => {
+      return (
+            <div className="partners-section container">
+                  <div className="partners-box">
+                        <div className="partners-title">
+                              Partners and Sponsors
+                        </div>
+                        <div className="partners-img row">
+                              {
+                                    partners.map((partner, idx) => {
+                                          return(
+                                                <div className="partners-col-1 col-lg-6 col-md-12">
+                                                      <img className="partners-logo" src={`${partner.img}`} alt="partners"/>
+                                                      <h2 className="logo-name">
+                                                            {partner.name}
+                                                      </h2>
+                                                </div>
+                                          )     
+                                    })
+                              }
+                        </div>
+                  </div>
+            </div>
+      );
 }
-
 export default PartnersSection;
