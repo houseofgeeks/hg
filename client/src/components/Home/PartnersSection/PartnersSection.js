ddc373c6b2991e2d229ac26ed01bc7b33556fb25
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-09-27 02:03:52 +0530

    updating partners section

--- client/src/components/Home/PartnersSection/PartnersSection.js
+++ client/src/components/Home/PartnersSection/PartnersSection.js
@@ -1,31 +1,37 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnersSection.css';
 import {PartnersList as partners} from './PartnersList.js';
 
 const PartnersSection = () => {
-      return (
-            <div className="partners-section container">
-                  <div className="partners-box">
-                        <div className="partners-title">
-                              Partners and Sponsors
-                        </div>
-                        <div className="partners-img row">
-                              {
-                                    partners.map((partner, idx) => {
-                                          return(
-                                                <div key={`${idx}`} className="partners-col-1 col-lg-6 col-md-12">
+      return (<div className="partners-section container">
+            <div className="partners-box">
+                  <div className="partners-title">
+                        Partners and Sponsors
+                  </div>
+                  <div className="partners-img row">
+                        {
+                              partners.map((partner, idx) => {
+                                    return (<div key={`${idx}`} className="partners-col-1 col-lg-6 col-md-12">
+                                          <div className="logo-container">
+                                                <Link target="_blank" to="/sponsors">
                                                       <img className="partners-logo" src={`${partner.img}`} alt="partners"/>
-                                                      <h2 className="logo-name">
-                                                            {partner.name}
-                                                      </h2>
-                                                </div>
-                                          )     
-                                    })
-                              }
-                        </div>
+                                                      <div className="logo-hover">
+                                                            <h2 className="logo-name">
+                                                                  {partner.name}
+                                                            </h2>
+
+                                                      </div>
+                                                </Link>
+
+                                          </div>
+
+                                    </div>)
+                              })
+                        }
                   </div>
             </div>
-      );
+      </div>);
 }
 export default PartnersSection;
