99d6d8928ca52b938b0822b61d76b9ea109c44c5
Abhay Raj <abhayrajsingh1707@gmail.com>
2020-09-29 00:47:43 +0530

    Remaking the 404 NotFound Page (#89)
    * Recreated error component
    
    * fixing overflow
    
    Co-authored-by: Hari Om Yadav <66957239+hariom1625@users.noreply.github.com>

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Error.css'
+import astronaut from './Images/astronaut.svg'
+import four from './Images/4.svg';
+import zero from './Images/0.svg';
+import error from './Images/error.svg'
+import not_found from './Images/page-not-found.svg'
+import planet from './Images/planet.svg'
 
 const Error = () => {
     return (
-        <div className="error-component container">
-            <hr />
-            <div className="text-center text-danger firstdiv">
-                <h1 className="font-weight-bolder" style={{ opacity: 1 }}>404 Error </h1>
-                <h4 className="font-weight-bolder">Page Not Found</h4>
-            </div>
-            <hr />
-            <div className="text-center seconddiv">
-                <div className="pad-topbot">
-                    <p className="whitecolor display-5">We're sorry , the page you requested could not be found</p>
-                    <p className="whitecolor">Please go back to the home page</p>
-                    <div className="text-center">
-                        <button type="button" className="btn btn-danger"><NavLink to="/" className=".text-muted">Back to home</NavLink></button>
+        <div className="error-component">
+            <div className="container-fluid mt-5 mx-auto">
+                <div className="stars">
+                    <NavLink className="btn-go-home" to="/" >HOME</NavLink>
+                    <img className="error" src={error} alt="error" width="200px" />
+                    <div className="error_message mx-auto">
+                        <img className="image-4" src={four} alt="error" />
+                        <img className="image-0" src={zero} alt="error" />
+                        <img className="image-404" src={four} alt="error" />
+                    </div>
+                    <div className="page_not_found" >
+                        <img className="not_found" src={not_found} alt="page not found" />
+                    </div>
+                    <div className="box_astronaut">
+                        <img className="object_astronaut" src={astronaut} alt="astrounet" />
+                    </div>
+                    <div className="planet" >
+                        <img className="object_planet" src={planet} alt="planet" />
                     </div>
                 </div>
             </div>
