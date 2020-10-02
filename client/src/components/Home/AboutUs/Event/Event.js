65118a88541e52a5899dd519d219860e5587c4ac
NiketanG <nikegulekar@gmail.com>
2020-10-03 01:27:16 +0530

    :lipstick: Updated Events Component
    Fixed Events Page

--- client/src/components/Home/AboutUs/Event/Event.js
+++ client/src/components/Home/AboutUs/Event/Event.js
@@ -2,27 +2,50 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import planet from "./planet.svg";
-import mobilePlanet from "./mobile-planet.svg";
 import "./Event.css";
 
 function Event() {
   const [events, setEvent] = useState([
     { title: "fetching events", description: ". . ." },
   ]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     axios
       .get("/api/event")
       .then((res) => {
         const { result } = res.data;
         setEvent([...result]);
+        setIsLoading(false);
       })
       .catch((err) => {
-        const result = [];
-        result.push({
-          title: "Something went wrong!",
-          description: "Try reloading. . .",
-        });
-        setEvent(result);
+        setIsLoading(false);
+        setEvent([
+          // {
+          //   eventLink: "/",
+          //   title: "Something went wrong!",
+          //   description: "Try reloading. . .",
+          // },
+          {
+            eventLink: "/",
+            title: "Hacktoberfest",
+            description: "OctoCon - Webinars and Workshops on different Techs!",
+          },
+          {
+            eventLink: "/",
+            title: "ACM-ICPC",
+            description: "What, Why and How? ICPC Webinar!",
+          },
+          {
+            eventLink: "/",
+            title: "ACM-ICPC",
+            description: "Intro to Git/Github",
+          },
+          {
+            eventLink: "/",
+            title: "Sept Long Discussion",
+            description: "Discussion on qustion from sept long Challenge!",
+          },
+        ]);
       });
   }, []);
 
@@ -31,48 +54,40 @@ function Event() {
       <div className="event-title">From the Geeks' Satellite..</div>
       <div className="event-content">
         <div className="title">
-          Recieving signals...{" "}
+          <span className="receiving">Recieving signals...</span>
           <span className="decrypting"> Decrypting...</span>
           <span className="latest-news"> Latest Events...</span>
         </div>
-        <div className="event-list">
-          <ul>
-            {events.map((event, idx) => {
-              return (
-                <li key={idx}>
-                  <div className="event-item">
-                    <a
+        <div className="planet">
+          <img src={planet} className="planet-img" alt="planet" />
+        </div>
+        {!isLoading && (
+          <div className="event-list">
+            <ul>
+              {events.map((event, idx) => {
+                return (
+                  <li key={idx}>
+                    <div className="event-item">
+                      <a
                         rel="noopener noreferrer"
                         href={`${event.eventLink}`}
                         target="_blank"
                         className="item-title"
-                    >
-                      <div>
-                        {event.title}
+                      >
+                        <div>{event.title}</div>
+                      </a>
+                      <div className="item-description">
+                        {event.description && <p>{event.description}</p>}
                       </div>
-                    </a>
-                    <div className="item-description">
-                      {event.description && <p>{event.description}</p>}
+                      <div className="item-link"></div>
                     </div>
-                    <div className="item-link">
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
-      <div className="planet">
-        <img src={planet} className="planet-img" alt="planet" />
-      </div>
-      <div className="mobile-planet">
-      <img
-         src={mobilePlanet}
-         className="mobile-planet-img"
-         alt="mobile planet"
-      />
-     </div>
     </div>
   );
 }
