2c4c3aa8c7c45da863529b25b18ddd1f5054a0a7
Dinesh S Y <50043613+dineshsy@users.noreply.github.com>
2020-10-02 14:45:33 +0530

    [Feat] Close the navbar on `esc` keypress #144 (#145)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import blackWave from "./side-bar.svg";
 
 function Navbar() {
-  
   const currentloc = String(window.location); //Gets current URL
   let currenttab = currentloc.substring(currentloc.lastIndexOf("/") + 1); //Gets current element from navbar
 
@@ -18,28 +17,38 @@ function Navbar() {
     //When visiting home section
     currenttab = "home";
   }
-  
+
   const [active, setActive] = useState(currenttab);
   const [navOpen, toggleNav] = useState(0);
 
   // Handling the case for back button press.
-  window.onpopstate=function()
-  {
-      const currentloc = String(window.location); //Gets current URL
-      let currenttab = currentloc.substring(currentloc.lastIndexOf("/") + 1); //Gets current element from navbar
+  window.onpopstate = function () {
+    const currentloc = String(window.location); //Gets current URL
+    let currenttab = currentloc.substring(currentloc.lastIndexOf("/") + 1); //Gets current element from navbar
 
-      if (currenttab[currenttab.length - 1] === "?") {
-        //When search bar is used, preventing bug of nothing being active in navbar
-        currenttab = currenttab.substring(0, currenttab.length - 1); //Removing ? sign when search bar used
-      }
+    if (currenttab[currenttab.length - 1] === "?") {
+      //When search bar is used, preventing bug of nothing being active in navbar
+      currenttab = currenttab.substring(0, currenttab.length - 1); //Removing ? sign when search bar used
+    }
 
-      if (currenttab.length === 0) {
-        //When visiting home section
-        currenttab = "home";
-      }
-      setActive(currenttab);
-  }
-    
+    if (currenttab.length === 0) {
+      //When visiting home section
+      currenttab = "home";
+    }
+    setActive(currenttab);
+  };
+
+  useEffect(() => {
+    const ESC_KEY_CODE = 27;
+    const toggleNavbarOnEsc = ({ keyCode }) => {
+      if (keyCode === ESC_KEY_CODE) toggleNav((navOpen) => navOpen && !navOpen);
+    };
+    window.addEventListener("keydown", toggleNavbarOnEsc);
+    return () => {
+      window.removeEventListener("keydown", toggleNavbarOnEsc);
+    };
+  }, [toggleNav]);
+
   return (
     <div>
       <div
