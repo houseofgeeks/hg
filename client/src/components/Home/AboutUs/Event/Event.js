55d5f350aeb9b7deb4d76f11ad6ebfc060597c30
NiketanG <nikegulekar@gmail.com>
2020-10-03 15:17:29 +0530

    :art: Rewrote Event Component Styles
    :bug: Fixed positioning bugs on Event Component for Event Page and Home Page

--- client/src/components/Home/AboutUs/Event/Event.js
+++ client/src/components/Home/AboutUs/Event/Event.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import planet from "./planet.svg";
 import "./Event.css";
+import useWindowDimensions from "../../../../utils/useWindowDimensions";
 
-function Event() {
+function Event({ page }) {
   const [events, setEvent] = useState([
     { title: "fetching events", description: ". . ." },
   ]);
@@ -23,50 +24,62 @@ function Event() {
           {
             eventLink: "/event",
             title: "Something went wrong!",
-            description: "Try reloading. . .",
+            description: "Try again....",
           },
         ]);
       });
   }, []);
 
+  const { height, width } = useWindowDimensions();
   return (
-    <div className="event-section">
-      <div className="event-title">From the Geeks' Satellite..</div>
-      <div className="event-content">
-        <div className="title">
-          <span className="receiving">Recieving signals...</span>
-          <span className="decrypting"> Decrypting...</span>
-          <span className="latest-news"> Latest Events...</span>
-        </div>
-        <div className="planet">
-          <img src={planet} className="planet-img" alt="planet" />
-        </div>
-        {!isLoading && (
-          <div className="event-list">
-            <ul>
-              {events.map((event, idx) => {
-                return (
-                  <li key={idx}>
-                    <div className="event-item">
-                      <a
-                        rel="noopener noreferrer"
-                        href={`${event.eventLink}`}
-                        target="_blank"
-                        className="item-title"
-                      >
-                        <div>{event.title}</div>
-                      </a>
-                      <div className="item-description">
-                        {event.description && <p>{event.description}</p>}
-                      </div>
-                      <div className="item-link"></div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+    <div>
+      <div className="planet">
+        <img
+          src={planet}
+          className={`planet-img ${page === "Home" ? "planet-img-home" : ""}`}
+          alt="planet"
+        />
+      </div>
+      <div
+        className={`event-section ${
+          page === "Home" ? "event-section-home" : ""
+        }`}
+      >
+        <div className="event-title">From the Geeks' Satellite..</div>
+        <div className="event-content">
+          <div className="title">
+            <span className="receiving">Recieving signals...</span>
+            <span className="decrypting"> Decrypting...</span>
+            <span className="latest-news"> Latest Events...</span>
           </div>
-        )}
+
+          {!isLoading && (
+            <div className="event-list">
+              <ul>
+                {events.map((event, idx) => {
+                  return (
+                    <li key={idx}>
+                      <div className="event-item">
+                        <a
+                          rel="noopener noreferrer"
+                          href={`${event.eventLink}`}
+                          target="_blank"
+                          className="item-title"
+                        >
+                          <div>{event.title}</div>
+                        </a>
+                        <div className="item-description">
+                          {event.description && <p>{event.description}</p>}
+                        </div>
+                        <div className="item-link"></div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
