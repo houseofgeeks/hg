45bbca6cb2c874dd3090179c32bafa673fe667ad
Ankit Raj <ankitatiiitr@gmail.com>
2020-10-24 22:26:12 +0530

    admin event mainupulation (#185)
    * admin funtionality added for Events
    
    * typo fixes
    
    * Added env variable for api

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import "./admin.css";
+import "react-toastify/dist/ReactToastify.css";
+import EventManage from "./event/EventManage";
+import CreateEvent from "./event/CreateEvent";
 
 const Admin = (props) => {
   const token = jwt_decode(window.localStorage.getItem("token"));
+  const [mode, updateMode] = useState("edit");
+  const history = useHistory();
+
   return (
     <div className="admin">
-      <h1>Admin Dashboard</h1>
+      <h1 style={{ paddingLeft: "1em" }}>Admin Dashboard</h1>
       <>
-        <div className="admin admin-details">
+        <div style={{ margin: ".5em auto" }} className="admin-details">
           <div>
-            <div style={{marginBottom: "10px"}}>
+            <div style={{ marginBottom: "10px" }}>
               <label>username: </label>
-              <b> {token.admin}</b>
-              <div className="logout-button" onClick={() => {
-								window.localStorage.clear();
-								props.history.push('/admin')
-							}}>Logout</div>
+              <b> {token.username}</b>
+              <div
+                className="logout-button"
+                onClick={() => {
+                  window.localStorage.clear();
+                  history.push("/admin");
+                }}
+              >
+                Logout
+              </div>
             </div>
             <div>
               <label>email: </label>
-							{/* to be updated when JWT will be updated on backend */}
+              {/* to be updated when JWT will be updated on backend */}
               <b> {token.email || "dummy@iiitranchi.ac.in"}</b>
             </div>
           </div>
         </div>
       </>
-      <div className="admin admin-tables">
-        <h1>componetnt to manipulate events and galary</h1>
+      <div>
+        <button
+          onClick={() => {
+            updateMode("create");
+          }}
+          disabled={mode === "create" ? true : false}
+          className="admin-create-button"
+        >
+          Create new Event
+        </button>
       </div>
+      {mode === "create" ? (
+        <div className="admin-create">
+          <CreateEvent updateMode={updateMode} />
+        </div>
+      ) : (
+        <div className="admin admin-tables">
+          <EventManage />
+        </div>
+      )}
     </div>
   );
 };
---
+++ client/src/components/Admin/event/EventManage.jsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Notify from "../../Notify";
+import { ToastContainer } from "react-toastify";
+import "./eventManage.css";
+import { MdDelete } from "react-icons/md";
+import { BiDetail } from "react-icons/bi";
+import EventModal from "./EditEvent";
+
+const EventManage = () => {
+  const [events, fillEvents] = useState([]);
+  const [loadStatus, updateLoadStatus] = useState({
+    message: "Loading . . .",
+    color: "#fff",
+  });
+  const [viewModal, showModal] = useState(-1);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API || ''}/event/all`)
+      .then((res) => {
+        fillEvents([...res.data.result]);
+        updateLoadStatus({ message: "Loaded" });
+      })
+      .catch((err) => {
+        Notify("Something went wrong! Try reloading", "err");
+        updateLoadStatus({
+          message: "Loading failed, Try RELOADING . . .",
+          color: "#ee2a7b",
+        });
+      });
+  }, []);
+
+  const updateEvent = (elem) => {
+    const payload = elem;
+    axios
+      .put(`${process.env.REACT_APP_API || ''}/event/${events[viewModal]._id}`, payload, {
+        headers: {
+          authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        let temp_data = [...events];
+        temp_data[viewModal] = elem;
+        fillEvents([...temp_data]);
+        Notify("Updated!", "success");
+      })
+      .catch((err) => {
+        const idx = viewModal;
+        Notify("Something went wrong! Try again", "err");
+        showModal(-1);
+
+        // Little hack to update state twice to unmount then again mount EditEvent component
+        setTimeout(() => {
+          showModal(idx);
+        }, 100);
+      });
+  };
+
+  const handleDelete = (_id, idx) => {
+    axios.delete(`${process.env.REACT_APP_API || ''}/event/${_id}`, {
+      headers: {
+        authorization: `Bearer ${window.localStorage.getItem("token")}`
+      }
+    })
+      .then((res) => {
+        const tempEvents = [...events];
+        tempEvents.splice(idx, 1);
+        fillEvents([...tempEvents]);
+        Notify("Event deleted!", "success");
+      })
+      .catch((err) => {
+        console.log(err);
+        Notify("Deleting Failed, try again!", "err");
+      })
+  }
+
+  return (
+    <>
+      {loadStatus.message !== "Loaded" && (
+        <h1 style={{ color: loadStatus.color, textAlign: "center" }}>
+          {loadStatus.message}
+        </h1>
+      )}
+      {loadStatus.message === "Loaded" && (
+        <>
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
+            <div style={{overflowY: 'scroll', maxHeight: '60vh', height: 'max-content', border: '2px solid #ed1c40', borderRadius: '.25rem'}}>
+              <table className="event-manage">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {events.map((elem, idx) => {
+                    return (
+                      <tr key={`${elem._id}`}>
+                        <td>{idx + 1}</td>
+                        <td>{elem.title}</td>
+                        <td
+                          onClick={() => {
+                            showModal(-1);
+                            setTimeout(() => {
+                              showModal(idx);
+                            }, 100);
+                          }}
+                          title="show/edit"
+                          className="edit button"
+                        >
+                          <BiDetail />
+                        </td>
+                        <td
+                          onClick={() => {
+                            handleDelete(elem._id, idx);
+                          }}
+                          label="delete"
+                          className="button delete"
+                        >
+                          <MdDelete />
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            {viewModal !== -1 && (
+              <div>
+                <EventModal
+                  updateEvent={updateEvent}
+                  showModal={showModal}
+                  elem={events[viewModal]}
+                />
+              </div>
+            )}
+          </div>
+        </>
+      )}
+      <ToastContainer autoClose={5000} />
+    </>
+  );
+};
+
+export default EventManage;
