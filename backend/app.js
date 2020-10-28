a7129ef67b19be68dedcd2fcd219daf473b7967b
kychok98 <kychok98@gmail.com>
2020-10-28 16:28:45 +0800

    fix deprecated warning: add useCreateIndex

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
-var eventRouter = require('./routes/event');
+const indexRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
+const eventRouter = require('./routes/event');
 
 mongoose.connect(process.env.MONGO_DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
