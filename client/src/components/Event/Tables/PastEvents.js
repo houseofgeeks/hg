d2b1e380cb577760ebaa1c84e7bcb4616347482d
EmperorYP7 <pandeyyash04@gmail.com>
2020-10-29 11:07:58 +0530

    12 hr format & icon

--- client/src/components/Event/Tables/PastEvents.js
+++ client/src/components/Event/Tables/PastEvents.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BiLinkExternal } from 'react-icons/bi';
 import './PastEvents.css';
 
 function PastEvents(props) {
@@ -14,6 +15,18 @@ function PastEvents(props) {
         if(props.events != null)
             setEvents(props.events);
     }, [props.events, events])
+
+    function tConvert(time) {
+        // Check correct time format and split into components
+        time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+        if (time.length > 1) { // If time format correct
+            time = time.slice (1);  // Remove full string match value
+            time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+            time[0] = +time[0] % 12 || 12; // Adjust hours
+        }
+        return time.join (''); // return adjusted time or original string
+    }
 
     return (
         <div className="table-2">
@@ -42,10 +55,11 @@ function PastEvents(props) {
                                                 href={`${event.eventLink}`}
                                                 target="_blank"
                                             >
-                                                {`${event.title}`}
+                                                {`${event.title} `}
+                                                <BiLinkExternal/>
                                             </a>
                                         </td>
-                                        <td>{`${date.toLocaleDateString()} | ${date.toLocaleTimeString().substring(0, 5)}`}</td>
+                                        <td>{`${date.toLocaleDateString() } | ${ tConvert(date.toLocaleTimeString().substr(0, 5))}`}</td>
                                         <td>{`${event.community}`}</td>
                                     </tr>
                                 )
