1cb0a24540867bd045a053d980204ce3244ed7d7
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-10-31 09:40:20 -0400

    promote issue-197

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -8,10 +8,10 @@ import loader from "./loader.svg";
 function Community() {
   return (
     <div className="community-component">
-      <div className="side-banner">Wings of House of Geeks</div>
-      <div className="title">Communities of House of Geeks</div>
+      <div className="side-banner">Wings of House of Geeks</div>      
       <div className="container-fluid main">
-        <div className="row justify-content-around">         
+        <div className="row justify-content-around"> 
+          <div className="title">Communities of House of Geeks</div>        
           <div className="col-12" />
           {list.map((item) => {
             return (
