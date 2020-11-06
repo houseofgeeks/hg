8b4e184089f86e6a6f4a39fc25605ba756f28621
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-11-06 12:08:45 -0500

    clear the tap and space

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -10,9 +10,9 @@ function Community() {
   let currenttab = currentloc.substring(currentloc.lastIndexOf("/") + 1); 
   return (
     <div className="community-component">
-      <div className="side-banner">Wings of House of Geeks</div>      
+      <div className="side-banner">Wings of House of Geeks</div>
       <div className="container-fluid main">
-        <div className="row justify-content-around"> 
+        <div className="row justify-content-around">
           {currenttab === "community" && ( <div className="title">Communities of House of Geeks</div> )}
           <div className="col-12" />
           {list.map((item) => {
