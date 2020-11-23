b5917fdb66394c17d9007a369818294cd4b8b035
EmperorYP7 <pandeyyash04@gmail.com>
2020-11-23 21:44:32 +0530

    Adding remaining logos (v1.0 completed)

--- client/src/components/Community/Communities/CommunityDetails.js
+++ client/src/components/Community/Communities/CommunityDetails.js
@@ -6,13 +6,13 @@ export const CommunityDetails = {
         "Technology has revolutionized the world and holds the key to our future. Department of Computer Science and Engineering, IIIT Ranchi , conducts various technical activities at regular intervals in association with Competitive coding community. The community arranges workshops focusing on various activities such as, basic programming, problem solving, online contest discussions etc. Programming competitions are frequently conducted to inculcate a healthy coding culture in the institute. Our objectives - Develop creative thinking, construct knowledge and develop innovative products. - Help students prepare for IT companies,coding rounds, prestigious competitive coding events like ACM ICPC.",
   },
   design: {
-    imageLink: "./images/CP_Community.svg",
+    imageLink: "./images/UIUX_Community.svg",
     name: "Community of Design UI and UX",
     details:
         "In today’s technology-driven world, phrases like responsive design and user-friendly interface have become increasingly important across industries. Now more than ever, companies need innovative thinkers with the ability to transform their businesses through optimized interfaces and seamless user experiences. UI/UX community lets student get a hands-on training and instruction in high-demand UX/UI design methodology, user-centric design research, design thinking, and web prototyping, interaction design with JavaScript and jQuery, and much more in the line. Students also get to apply what they’ve learned towards building a comprehensive portfolio of projects ranging from AI sketches to mobile-responsive web layouts.",
   },
   machine_learning: {
-    imageLink: "./images/CP_Community.svg",
+    imageLink: "./images/ML_Community.svg",
     name: "Community of Machine Learning",
     details:
       "Machine learning community of IIIT Ranchi aims at building and fostering a vibrant machine learning community. Our members gain a unique opportunity to develop machine learning solutions tailored towards relevant and specific industry challenges. We believe in lowering the barrier of entry to the field of machine learning and strive to make machine learning education as accessible as possible on the IIIT Ranchi campus and beyond. We host both technical and non-technical student-run courses on the campus teaching the fundamentals of state of the art machine learning techniques as well as the growing societal and ethical implications of artificial intelligence. Join us and become a part of future makers.",
