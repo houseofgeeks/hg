1926928898c6edfcb90da12930c8a7377007b424
Ankit Raj <ankitatiiitr@gmail.com>
2020-12-28 23:31:42 +0530

    bug-fix: added try catch block to contributors fetch

--- client/src/components/Footer/modal.js
+++ client/src/components/Footer/modal.js
@@ -4,19 +4,30 @@ import "./modal.css";
 import ShowName from "./showName";
 
 function Modal() {
+  // 1 - while fetching, 0 - fetch failed, -1 - fetched, 
+  const [dataStatus, updateStatus] = useState({status: 1, message: "Fetching Contributors form GitHub"});
   const [userData, setUserData] = useState([]);
 
   useEffect(() => {
+    const getGitHubUserWithFetch = async () => {
+      try{
+        const response = await fetch(
+          "https://api.github.com/repos/houseofgeeks/hg/contributors"
+        );
+        if(response.status === 200){
+          const jsonData = await response.json();
+          console.log(response);
+          setUserData(jsonData);
+          updateStatus({status: -1, message: "Fetched"});
+        }else{
+          updateStatus({status: 0, message: "Fetching failed, try after after some time"});
+        }
+      }catch(e){
+        updateStatus({status: 0, message: "Fetching failed, try after after some time"});
+      }
+    };
     getGitHubUserWithFetch();
   }, []);
-
-  const getGitHubUserWithFetch = async () => {
-    const response = await fetch(
-      "https://api.github.com/repos/houseofgeeks/hg/contributors"
-    );
-    const jsonData = await response.json();
-    setUserData(jsonData);
-  };
 
   return (
     <div
@@ -35,31 +46,39 @@ function Modal() {
             <p type="button" className="close-button" data-dismiss="modal">
               <span>x</span>
             </p>
-
-            {userData.map((item) => (
-              <div key={item.id} className="col-lg-4 col-md-6 col-12">
-                <div className="contributor-container ">
-                  <div className="image-container">
-                    <img
-                      className="contributor-image"
-                      src={item.avatar_url}
-                      alt="items"
-                    />
-                    <a
-                      className=" image-hover"
-                      target="_blank"
-                      href={item.html_url}
-                      rel="noopener noreferrer"
-                    >
-                      <h2 className="username">{item.login}</h2>
-                    </a>
+            {dataStatus.status === -1 ?  
+              <>{userData.map((item) => (
+                <div key={item.id} className="col-lg-4 col-md-6 col-12">
+                  <div className="contributor-container ">
+                    <div className="image-container">
+                      <img
+                        className="contributor-image"
+                        src={item.avatar_url}
+                        alt="items"
+                      />
+                      <a
+                        className=" image-hover"
+                        target="_blank"
+                        href={item.html_url}
+                        rel="noopener noreferrer"
+                      >
+                        <h2 className="username">{item.login}</h2>
+                      </a>
+                    </div>
+                    <ShowName prop={item.url}></ShowName>
                   </div>
-                  <ShowName prop={item.url}></ShowName>
                 </div>
-              </div>
-            ))}
-          </div>
-
+              ))}</>
+              : 
+              <>
+                <div className="contributor-message">
+                  <img alt="github-error" height={200} width={200} src="./images/github_error.png" />
+                  <br />
+                  <div>{dataStatus.message}</div>
+                </div>
+              </>
+            }
+            </div>
           <div className="modal-footer">
             <p className="text-white">
               Want to Contribute ? Visit
