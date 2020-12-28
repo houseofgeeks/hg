34cd0477d9c172916ac90df4b5281af7a43321a1
Soumyabrata Bairagi <69338695+sb2356-iiitr@users.noreply.github.com>
2020-12-28 14:45:56 +0400

    Update CommunityDetails.js
    Updated links for respective images.

--- client/src/components/Community/Communities/CommunityDetails.js
+++ client/src/components/Community/Communities/CommunityDetails.js
@@ -1,36 +1,36 @@
 export const CommunityDetails = {
   competitive_coding: {
-    imageLink: "./images/CP_Community.svg",
+    imageLink: "./images/Communities/CP_Community.svg",
     name: "Community of Competitive Coding",
     details:
         "Technology has revolutionized the world and holds the key to our future. Department of Computer Science and Engineering, IIIT Ranchi , conducts various technical activities at regular intervals in association with Competitive coding community. The community arranges workshops focusing on various activities such as, basic programming, problem solving, online contest discussions etc. Programming competitions are frequently conducted to inculcate a healthy coding culture in the institute. Our objectives - Develop creative thinking, construct knowledge and develop innovative products. - Help students prepare for IT companies,coding rounds, prestigious competitive coding events like ACM ICPC.",
   },
   design: {
-    imageLink: "./images/UIUX_Community.svg",
+    imageLink: "./images/Communities/UIUX_Community.svg",
     name: "Community of Design UI and UX",
     details:
         "In today’s technology-driven world, phrases like responsive design and user-friendly interface have become increasingly important across industries. Now more than ever, companies need innovative thinkers with the ability to transform their businesses through optimized interfaces and seamless user experiences. UI/UX community lets student get a hands-on training and instruction in high-demand UX/UI design methodology, user-centric design research, design thinking, and web prototyping, interaction design with JavaScript and jQuery, and much more in the line. Students also get to apply what they’ve learned towards building a comprehensive portfolio of projects ranging from AI sketches to mobile-responsive web layouts.",
   },
   machine_learning: {
-    imageLink: "./images/ML_Community.svg",
+    imageLink: "./images/Communities/ML_Community.svg",
     name: "Community of Machine Learning",
     details:
       "Machine learning community of IIIT Ranchi aims at building and fostering a vibrant machine learning community. Our members gain a unique opportunity to develop machine learning solutions tailored towards relevant and specific industry challenges. We believe in lowering the barrier of entry to the field of machine learning and strive to make machine learning education as accessible as possible on the IIIT Ranchi campus and beyond. We host both technical and non-technical student-run courses on the campus teaching the fundamentals of state of the art machine learning techniques as well as the growing societal and ethical implications of artificial intelligence. Join us and become a part of future makers.",
   },
   robotics: {
-    imageLink: "./images/Robotics_Community.svg",
+    imageLink: "./images/Communities/Robotics_Community.svg",
     name: "Community of Robotics",
     details:
       "Does that image of R2D2 inspire you to create a personal one for yourself? If you have ever even secretly aspired to build your own robot, then the Robotics Community, IIIT Ranchi is the place for you! One on one guidance, workshops and tutorials along with tools, equipments, components and workspace eagerly awaits you! The club strives to impart knowledge in the ever growing field of robotics via mini hands-on activities as well as giving students exposure in the form of lectures from the experts as well as through competitions.",
   },
   electronics_iot: {
-    imageLink: "./images/Elec_Community.svg",
+    imageLink: "./images/Communities/Elec_Community.svg",
     name: "Community of Electronics and IoT",
     details:
       "Electronics and IoT community has the sole objective of inculcating a spirit of developing innovative technology solutions to real life problems. We provide a platform to the students’ body where any individual with an idea can approach the club freely to grasp the necessary technical skills required to turn that idea into a reality. This is a place where students get an opportunity to think outside the academic curriculum and get practical experience by implementing and applying concepts learnt in various theoretical courses. We conduct a plethora of lectures, workshops, projects as well as competitions throughout the year concerning both analog as well as digital electronics keep the calendar busy and the participants, learning. An Emerging Technological Innovation. Internet of Things (IoT) is creating an environment of convergence in society. This technology environment brings a paradigm shift in our professional and personal life. The community also encourages the students of the institute in the Internet of Things and related domains.",
   },
   webd_appd: {
-    imageLink: "./images/WebD_Community.svg",
+    imageLink: "./images/Communities/WebD_Community.svg",
     name: "Community of Web and App Development",
     details:
       "Web and application development community of IIIT Ranchi has been primarily responsible for providing innovative technical solutions to most of the issues in the institute. We began as web development Club with the sole purpose of developing web applications . But after enormous support from students we have now evolved into a club that has proficient developers from the field of Android Development, Web Development, Game Development and IOS development. All the four verticals work in harmony to develop innovative softwares that solve many real life technical issues on campus.",
