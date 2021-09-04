40c8a23b6de9da3eb53e4d138087ce0499108352
Srijan Srivastava <79690889+SrijanSriv@users.noreply.github.com>
2021-09-04 12:27:32 +0530

    Update ElcMembersList.js

--- client/src/components/Members/Community/ElcMembersList.js
+++ client/src/components/Members/Community/ElcMembersList.js
@@ -9,13 +9,5 @@ export const ElcMembersLists = [
             linkedin: 'https://www.linkedin.com/in/shashwat-pratap-470b7116b',
             github: 'https://github.com/shashwat0426'
       }
-      // ,{
-      //       id: 1,
-      //       img: './images/Members/Core Electronics/devansh.jfif',
-      //       name: 'Devansh Srivastava',
-      //       position: 'Community Co-ordinator',
-      //       mail: 'devansh.btech.ec18@iiitranchi.ac.in',
-      //       linkedin: 'https://www.linkedin.com/in/devansh-srivastava-958766167'
-      // }
 
 ];
